c8dc693f6688f7987cb77ad2e8f2b31f1cf117fd
cottet julien <julien.cottet@group.leguide.com>
2019-12-02 23:21:00 +0100

    Ex 2.5 simplify types

--- src/test/scala/chapters/ch2/CompositionTest.scala
+++ src/test/scala/chapters/ch2/CompositionTest.scala
@@ -6,20 +6,20 @@ class CompositionTest extends org.scalatest.FunSuite {
     val composition: Composition = new Composition
     val cross2: Int => Int = a => a * 2
     val plus3: Int => Int = a => a + 3
-    assert(composition.compose[Int, Int, Int](cross2, plus3)(1) === 8)
+    assert(composition.compose(cross2, plus3)(1) === 8)
   }
 
   test("1 * 2 + 3) = 5") {
     val composition: Composition = new Composition
     val cross2: Int => Int = a => a * 2
     val plus3: Int => Int = a => a + 3
-    assert(composition.compose[Int, Int, Int](plus3, cross2)(1) === 5)
+    assert(composition.compose(plus3, cross2)(1) === 5)
   }
 
   test("f(a => print(a)), g(a => 2*a) => a => 2a") {
     val composition: Composition = new Composition
     val print: Int => String = a => "%d".format(a)
     val mult: Int => Int = b => b * 2
-    assert(composition.compose[Int, Int, String](print, mult)(1) === "2")
+    assert(composition.compose(print, mult)(1) === "2")
   }
 }
